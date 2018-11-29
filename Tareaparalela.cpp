0cc2d3615ce3e42cb6009779c9ae19b0dd3e31e0
Alexis Burgos Oyaneder <38987071+ABurgosOyaneder@users.noreply.github.com>
2018-11-29 11:00:44 -0300

    Add files via upload
    creacion de archivo, en blanco

---
+++ Tareaparalela.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<cstdlib>
+#include<fstream>
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+}
+
+
+
