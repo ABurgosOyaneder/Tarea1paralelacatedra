503341e5e91ab3254bebc1be9da2e9ab61c11951
Alexis Burgos Oyaneder <38987071+ABurgosOyaneder@users.noreply.github.com>
2018-11-29 12:45:27 -0300

    Update Tareaparalela.cpp
    creacion de archivos para dividir numeros.txt

--- Tareaparalela.cpp
+++ Tareaparalela.cpp
@@ -8,11 +8,22 @@
 using namespace std;
 
 
-
 int main(int argc, char const *argv[])
 {
+    float cadena[128];
+    //crear fichero de salida
+    ofstream fs("rango 1.txt");
+    ofstream fs("rango 2.txt");
+    ofstream fs("rango 3.txt");
+    ofstream fs("rango 4.txt");
+    ofstream fs("rango 5.txt");
+    ofstream fs("rango 6.txt");
+    ofstream fs("rango 7.txt");
+
     
+
 }
+
 
 
 
